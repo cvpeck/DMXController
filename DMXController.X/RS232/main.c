e5c21b4abea7336cb76a0798684fc0b0d66509d5
Chris Peck <chris.peck@internode.on.net>
2017-03-23 09:01:51 +1030

    Initial commit

---
+++ DMXController.X/RS232/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   newmain.c
+ * Author: peckc
+ *
+ * Created on March 18, 2017, 9:39 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ DMXController.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DMXController.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DMXController.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dmxcontroller.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dmxcontroller.x.tar
